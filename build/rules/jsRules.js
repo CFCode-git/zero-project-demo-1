86b90aaab2fa241486fb02a255e934ec13d46fd1
CFCode-git <435184766@qq.com>
2021-02-18 14:33:38 +0800

    成功运行项目

---
+++ build/rules/jsRules.js
@@ -0,0 +1,21 @@
+const { resolve } = require("./../utils");
+
+module.exports = [
+  {
+    test: /\.(j|t)sx?$/,
+    use: [
+      {
+        loader: "babel-loader",
+        options: {
+          babelrc: false,
+          presets: ["@babel/preset-typescript", "@babel/preset-react"],
+          plugins: [
+            ["@babel/plugin-proposal-decorators", { legacy: true }],
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
+            "@babel/plugin-syntax-dynamic-import",
+          ],
+        },
+      },
+    ],
+  },
+];
