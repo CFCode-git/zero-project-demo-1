a61382752bab83ba5d21a4fd334030b5d13948d3
CFCode-git <435184766@qq.com>
2021-02-18 15:01:46 +0800

    安装antd, 配合bable-plugin-import按需加载

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from "antd";
 
 import styles from "./index.scss";
 
 const render = () => {
   ReactDOM.render(
-    <div className={styles.test}>123</div>,
+    <div className={styles.test}>
+      123
+      <Button type="primary">This is a Button from antd</Button>
+    </div>,
     document.querySelector("#app")
   );
 };
