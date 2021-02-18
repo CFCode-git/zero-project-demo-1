6e21b9b08582f05591743a0dcc0b6cd78b68a550
CFCode-git <435184766@qq.com>
2021-02-18 15:18:16 +0800

    添加公共组件shared的demo和业务页面views的demo

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,19 @@ import { Button } from "antd";
 
 import styles from "./index.scss";
 
+import ViewTest from "./containers/views/ViewsTest/index.tsx";
+import SharedTest from "./containers/shared/SharedTest/index.tsx";
+
 const render = () => {
   ReactDOM.render(
     <div className={styles.test}>
       123
       <Button type="primary">This is a Button from antd</Button>
+      <hr />
+      <div>
+        <ViewTest />
+        <SharedTest />
+      </div>
     </div>,
     document.querySelector("#app")
   );
