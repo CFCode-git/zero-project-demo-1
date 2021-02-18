966aa519c472555590006f02904b8f7aff000e30
CFCode-git <435184766@qq.com>
2021-02-18 15:06:00 +0800

    安装less less-loader, 用于antd添加样式

--- build/rules/styleRules.js
+++ build/rules/styleRules.js
@@ -25,4 +25,19 @@ module.exports = [
       },
     ],
   },
+  {
+    test: /\.less$/,
+    use: [
+      "style-loader",
+      "css-loader",
+      {
+        loader: "less-loader",
+        options: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+      },
+    ],
+  },
 ];
