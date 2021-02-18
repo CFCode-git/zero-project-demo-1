940232ca6c81992470a164472248711fc8c54249
CFCode-git <435184766@qq.com>
2021-02-18 14:49:40 +0800

    css-module优化, 带上原本的类名

--- build/rules/styleRules.js
+++ build/rules/styleRules.js
@@ -6,7 +6,9 @@ module.exports = [
       {
         loader: "css-loader",
         options: {
-          modules: true,
+          modules: {
+            localIdentName: "[local]__[hash:base64:10]",
+          },
         },
       },
       {
