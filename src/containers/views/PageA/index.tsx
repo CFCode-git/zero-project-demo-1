971a6b6605161a37793e0514bec37b2e5c001e94
CFCode-git <435184766@qq.com>
2021-02-19 14:46:06 +0800

    配置eslint和prettier

--- src/containers/views/PageA/index.tsx
+++ src/containers/views/PageA/index.tsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { Button } from "antd";
-import { withRouter, RouteComponentProps } from "react-router-dom";
-import ShowCount from "@components/ShowCount";
-import CountOperation from "@components/CountOperation";
-import { getArticleList } from "@services/api";
+import React from 'react'
+import { Button } from 'antd'
+import { withRouter, RouteComponentProps } from 'react-router-dom'
+import ShowCount from '@components/ShowCount'
+import CountOperation from '@components/CountOperation'
+import { getArticleList } from '@services/api'
 
-import styles from "./page-a.scss";
+import styles from './page-a.scss'
 
 const PageA = ({ history }: RouteComponentProps) => {
-  const getList = async () => {
-    try {
-      const res = await getArticleList();
-      console.log(res);
-    } catch (error) {
-      console.log("error", error);
+    const getList = async () => {
+        try {
+            const res = await getArticleList()
+            console.log(res)
+        } catch (error) {
+            console.log('error', error)
+        }
     }
-  };
-  return (
-    <div>
-      <div>PageA</div>
-      <Button type="primary" onClick={() => history.push("/page-b")}>
-        Go To B
-      </Button>
-      <hr />
-      <ShowCount />
-      <CountOperation />
-      <hr />
-      <div>
-        <Button onClick={getList}>获取文章列表</Button>
-      </div>
-      <hr />
-      <div className={styles["page-a"]}>page-a css</div>
-      <hr />
-      <div>{process.env.NODE_ENV}</div>
-    </div>
-  );
-};
+    return (
+        <div>
+            <div>PageA</div>
+            <Button type="primary" onClick={() => history.push('/page-b')}>
+                Go To B
+            </Button>
+            <hr />
+            <ShowCount />
+            <CountOperation />
+            <hr />
+            <div>
+                <Button onClick={getList}>获取文章列表</Button>
+            </div>
+            <hr />
+            <div className={styles['page-a']}>page-a css</div>
+            <hr />
+            <div>{process.env.NODE_ENV}</div>
+        </div>
+    )
+}
 
-export default withRouter(PageA);
+export default withRouter(PageA)
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Button } from "antd";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'antd'
 
-import styles from "./index.scss";
+import styles from './index.scss'
 
-import ViewTest from "@views/ViewsTest/index";
-import SharedTest from "@shared/SharedTest/index";
+import ViewTest from '@views/ViewsTest/index'
+import SharedTest from '@shared/SharedTest/index'
 
-import App from "@views/App/app";
-import { Provider } from "@store/index";
+import App from '@views/App/app'
+import { Provider } from '@store/index'
 
 const render = () => {
-  ReactDOM.render(
-    <div className={styles.test}>
-      123
-      <Button type="primary">This is a Button from antd</Button>
-      <hr />
-      <div>
-        <ViewTest />
-        <SharedTest />
-      </div>
-      <hr />
-      <div>
-        <Provider>
-          <App />
-        </Provider>
-      </div>
-    </div>,
-    document.querySelector("#app")
-  );
-};
+    ReactDOM.render(
+        <div className={styles.test}>
+            123
+            <Button type="primary">This is a Button from antd</Button>
+            <hr />
+            <div>
+                <ViewTest />
+                <SharedTest />
+            </div>
+            <hr />
+            <div>
+                <Provider>
+                    <App />
+                </Provider>
+            </div>
+        </div>,
+        document.querySelector('#app')
+    )
+}
 
-render();
+render()
