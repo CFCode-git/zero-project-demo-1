41e513fb3e0857d66588d14d0d06e1c58994a910
CFCode-git <435184766@qq.com>
2021-02-19 11:09:04 +0800

    支持Axios

--- src/containers/views/PageA/index.tsx
+++ src/containers/views/PageA/index.tsx
@@ -3,8 +3,17 @@ import { Button } from "antd";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 import ShowCount from "@components/ShowCount";
 import CountOperation from "@components/CountOperation";
+import { getArticleList } from "@services/api";
 
 const PageA = ({ history }: RouteComponentProps) => {
+  const getList = async () => {
+    try {
+      const res = await getArticleList();
+      console.log(res);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
   return (
     <div>
       <div>PageA</div>
@@ -14,6 +23,10 @@ const PageA = ({ history }: RouteComponentProps) => {
       <hr />
       <ShowCount />
       <CountOperation />
+      <hr />
+      <div>
+        <Button onClick={getList}>获取文章列表</Button>
+      </div>
     </div>
   );
 };
